14020db2b4bb282351f1c43446168e012336ce93
lee <62833781+Liveshkumar@users.noreply.github.com>
2021-06-26 07:18:52 +0530

    Update huffman.py

--- huffman.py
+++ huffman.py
@@ -9,13 +9,7 @@ class node:
         self.left= left
         self.right= right
         self.code=''
-dict={}
-decode_dict={}
-def get_frequency(text):
-	for i in text:
-		if i not in dict:
-			dict[i]=0
-		dict[i]+=1
+
 encode_bit={}
 def generate_bit(node, val=''):
 	bit = val + str(node.code)
@@ -25,6 +19,7 @@ def generate_bit(node, val=''):
 		generate_bit(node.right, bit)
 	if(not node.left and not node.right):
 		encode_bit[node.char]=bit
+		
 def get_encode(txt):
 	encode_text=""
 	for i in txt:
@@ -33,21 +28,28 @@ def get_encode(txt):
 	return encode_text
 def padding(encoded_txt):
 	extra_pad=8-len(encoded_txt)%8
-	print(extra_pad)
+	#print(extra_pad)
 	for i in range(extra_pad):
 		encoded_txt += "0"
 	padding_val="{0:08b}".format(extra_pad)
 	encoded_txt=padding_val+encoded_txt
 	return encoded_txt
+# get frequency of char function
+def get_frequency(text):
+	freq={}
+	for i in text:
+		if i not in freq:
+			freqi]=0
+		freq[i]+=1
+	return freq
 def encode(input_file, output_file):
 	print("encoding ", input_file, output_file)
 	file1 = open(input_file,"r")
 	text=file1.read()
-	#text=text.rstrip()
-	get_frequency(text)
+	freq=get_frequency(text)
 	BST=[]
-	for i in dict:
-		BST.append(node(i,dict[i]))
+	for i in freq:
+		BST.append(node(i,freq[i]))
 	while(len(BST)>1):
 		nodes = sorted(BST, key=lambda x: x.freq)
 		left=nodes[0]
@@ -66,14 +68,18 @@ def encode(input_file, output_file):
 	encoded_txt=padding(encode_text)
 	#print(len(encoded_txt)%8)
 	characters = ""
-
+	for i in range(0, len(encoded_txt), 8):
+		char_int = int(encoded_txt[i:i+8], 2)
+		#print(char_int,end=" ")
+		characters += chr(char_int+1)
+	decode_dict={}
 	for i in encode_bit:
 		decode_dict[encode_bit[i]]=i
 	# storing length of encode bit + encode bit values + encoded chars
-	file3=open("dict.txt",'w')
+	file3=open("decode.txt",'w')
 	file3.write(str(decode_dict))
 	file2=open(output_file,'w')
-	file2.write(encoded_txt)
+	file2.write(characters)
 	file2.close()
 	file1.close()
 
@@ -83,15 +89,11 @@ def get_padding_val(text):
 	pad_val=int(char)
 	#print(pad_val)
 	return pad_val
-def decode_char(pad_Val,text):
+def decode_char(text):
 	decode_text=""
-	for i in text[pad_Val:]:
-		char=ord(i)
-		#print(char,end =" ")
-		#c=bin(char).replace("0b", "")
+	for i in text:
+		char=ord(i)-1
 		c="{0:08b}".format(char)
-		# while (len(c)) <8 :
-		# 	c='0'+c
 		decode_text+=c
 	return decode_text
 def removed_padding(decode_text,padval):
@@ -106,25 +108,21 @@ def decode_huffambit(decode_text,decode_dict):
 		if ch in decode_dict:
 			encode_char+=decode_dict[ch]
 			ch=""
-	print(encode_char)
 	return encode_char
+
 def decode(input_file, output_file):
 	print("decoding ", input_file, output_file)
 	file1=open(input_file,'r')
 	file2=open(output_file,'w')
-	file3=open("dict.txt",'r')
-	dict=file3.read()
+	file3=open("decode.txt",'r')
+	dict1=file3.read()
 	text=file1.read()
-	dict=eval(dict)
-	print(dict)
-	# getting the length of decode char
+	dict1=eval(dict1)
 	pad_val=get_padding_val(text)
-	print(pad_val)
-	decode_text=removed_padding(text,pad_val)
-	# decode_text=decode_char(1,decode_text)
-	encode_char=decode_huffambit(decode_text,dict)
-	print(encode_char)
-	file2.write(encode_char)
+	decode_text=decode_char(text[1:])
+	decode_text=removed_padding(decode_text,pad_val)
+	encode_char=decode_huffambit(decode_text,dict1)
+	file2.write(encode_char+'\n')
 	file2.close()
 	file3.close()
 	file1.close()
@@ -146,3 +144,5 @@ if __name__ == "__main__":
 		encode(options.e, options.o)
 	if options.d is not None:
 		decode(options.d, options.o)
+
+		
